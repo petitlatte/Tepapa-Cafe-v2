244d2f811af8af7222a157ef5787acfab87ef961
petitlatte <lisasumida@gmail.com>
2018-06-02 13:00:50 +1200

    latest version

--- js/script.js
+++ js/script.js
@@ -1,12 +1,42 @@
-window.onscroll = function() {fixedNavFunction()};
+// NAVBAR FUNCTION 
 
-var header = document.getElementById("stickynav");
-var sticky = header.offsetTop;
+$(window).scroll(function () {
+  console.log($(window).scrollTop())
+  if ($(window).scrollTop() > 63) {
+    $('.navbar').addClass('navbar-fixed');
+  }
+  if ($(window).scrollTop() < 64) {
+    $('.navbar').removeClass('navbar-fixed');
+  }
+});
 
-function fixedNavFunction() {
-  if (window.pageYOffset >= sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
+//POP UP FUNCTION 
+
+function popupFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+
+// MODAL BUG FIX
+$(document).on('click', '.add', function() {
+  $('#CartContainer').show();
+});
+
+
+//PROGRESS BAR
+
+function barFunction() {
+  var elem = document.getElementById("progress");   
+  var width = 0;
+  var id = setInterval(frameFunction, 1000);
+  function frameFunction() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      document.getElementById("progress-number").innerHTML = width * 1  + '%';
+    }
   }
 }
+
